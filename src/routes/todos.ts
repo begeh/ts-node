5339df1196dcd2efef7f94432fb4574ab7c9e149
Bashir <b_egeh@hotmail.com>
2021-01-01 15:42:45 -0500

    set up express server and todo routes

---
+++ src/routes/todos.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+
+// const express = require('express');
+// const Router = express.Router;
+
+const router = Router();
+
+router.post('/');
+
+router.get('/');
+
+router.patch('/:id');
+
+router.delete('/:id');
+
+export default router;
